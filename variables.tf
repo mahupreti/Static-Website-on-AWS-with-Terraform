77ad00bab81309f985a90689461d2f7d48f3aecd
mahupreti <yogeshupreti55@gmail.com>
2023-06-03 17:54:20 +0545

    first commit

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "region" {
+  type = string
+}
+variable "bucket_name" {
+  type = string
+}
+variable "domain_name" {
+  type = string
+}
+variable "alternative_name" {
+  type = string
+}
